e018e203e5d983959c3e44d63b357d074d7e62f3
lmonilsson <121645+lmonilsson@users.noreply.github.com>
2021-12-11 16:15:34 +0100

    Day 8, part 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -8,6 +8,10 @@
 
 List<Entry> LoadInput()
 {
+    //var input = new[]
+    //{
+    //    "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+    //};
     //var input = new string[]
     //{
     //    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
@@ -37,7 +41,57 @@ void Part1()
 
 void Part2()
 {
+    var digits = new Dictionary<string, int>()
+    {
+        { "abcefg", 0 },
+        { "cf", 1 },
+        { "acdeg", 2 },
+        { "acdfg", 3 },
+        { "bcdf", 4 },
+        { "abdfg", 5 },
+        { "abdefg", 6 },
+        { "acf", 7 },
+        { "abcdefg", 8 },
+        { "abcdfg", 9 }
+    };
+
     var entries = LoadInput();
+
+    long outputSum = 0;
+    foreach (var entry in entries)
+    {
+        var signals = entry.Signals;
+        var map = new Dictionary<char, char>();
+
+        var d1 = signals.First(x => x.Length == 2);
+        var d7 = signals.First(x => x.Length == 3);
+        map['a'] = d7.Except(d1).Single();
+
+        var d4 = signals.Single(x => x.Length == 4);
+        var d3 = signals.Single(x => x.Length == 5 && x.Intersect(d1).Count() == 2 && x.Intersect(d4).Count() == 3);
+        map['b'] = d4.Except(d1).Except(d3).Single();
+        map['d'] = d3.Intersect(d4).Except(d1).Single();
+
+        var d9 = signals.Single(x => x.Length == 6 && x.Intersect(d1).Count() == 2 && x.Contains(map['a']) && x.Contains(map['b']) && x.Contains(map['d']));
+        map['g'] = d9.Except(d1).Where(c => c != map['a'] && c != map['b'] && c != map['d']).Single();
+
+        var d6 = signals.Single(x => x.Length == 6 && x.Intersect(d1).Count() == 1);
+        map['e'] = d6.Except(d1).Where(c => c != map['a'] && c != map['b'] && c != map['d'] && c != map['g']).Single();
+        map['f'] = d6.Intersect(d1).Single();
+        map['c'] = d1.Except(d6).Single();
+
+        long output = 0;
+        foreach (var outp in entry.Outputs)
+        {
+            var decodedOutp = new string(outp.Select(c => map.First(kvp => kvp.Value == c).Key).OrderBy(c => c).ToArray());
+            output *= 10;
+            output += digits[decodedOutp];
+        }
+
+        outputSum += output;
+    }
+
+    Console.WriteLine($"Part 2: {outputSum}");
 }
 
 Part1();
@@ -50,7 +104,7 @@ class Entry
 
     public Entry(IReadOnlyList<string> signals, IReadOnlyList<string> outputs)
     {
-        Signals = signals;
-        Outputs = outputs;
+        Signals = signals.Select(x => new string(x.OrderBy(c => c).ToArray())).ToList();
+        Outputs = outputs.Select(x => new string(x.OrderBy(c => c).ToArray())).ToList();
     }
 }
