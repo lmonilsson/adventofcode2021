cfc1297d2994a82831589f6be6fa1705450e55cd
Linus Nilsson <linus.nilsson@trimma.se>
2021-12-15 13:19:12 +0100

    Day 14, part 2

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,27 +1,27 @@
 ﻿(string Template, Rule[] Rules) LoadInput()
 {
-    var input = new[]
-    {
-        "NNCB",
-        "",
-        "CH -> B",
-        "HH -> N",
-        "CB -> H",
-        "NH -> C",
-        "HB -> C",
-        "HC -> B",
-        "HN -> C",
-        "NN -> C",
-        "BH -> H",
-        "NC -> B",
-        "NB -> B",
-        "BN -> B",
-        "BB -> N",
-        "BC -> B",
-        "CC -> N",
-        "CN -> C"
-    };
-    //var input = File.ReadAllLines("input.txt");
+    //var input = new[]
+    //{
+    //    "NNCB",
+    //    "",
+    //    "CH -> B",
+    //    "HH -> N",
+    //    "CB -> H",
+    //    "NH -> C",
+    //    "HB -> C",
+    //    "HC -> B",
+    //    "HN -> C",
+    //    "NN -> C",
+    //    "BH -> H",
+    //    "NC -> B",
+    //    "NB -> B",
+    //    "BN -> B",
+    //    "BB -> N",
+    //    "BC -> B",
+    //    "CC -> N",
+    //    "CN -> C"
+    //};
+    var input = File.ReadAllLines("input.txt");
 
     var template = input.First();
     var rules = input.Skip(2).Select(Rule.Parse).ToArray();
@@ -42,22 +42,10 @@ void Part1()
 void Part2()
 {
     var (template, rules) = LoadInput();
-    //var polymer = BuildPolymer(template, rules, 40);
-    //var elementCounts = polymer.GroupBy(e => e);
-    //var lowestCount = elementCounts.MinBy(g => g.LongCount())!.LongCount();
-    //var highestCount = elementCounts.MaxBy(g => g.LongCount())!.LongCount();
-    //Console.WriteLine($"Part 2: {highestCount - lowestCount}");
-
-    /* Ideas:
-     *
-     * - Does it repeat? Can one stop after it has repeated, and do some multiplication to get the result?
-     *   No, it does not seem to repeat (tried 20 iterations).
-     *
-     * - It it possible to see which rules will be most and least common, and throw away everything that doesn't lead there?
-     *   No, this wouldn't help, since the number of occurences of the two elements is still too large.
-     *
-     * - Simple maths to get the number of expansion of the specific elements?
-     */
+    var elementCounts = BuildPolymerElementCounts(template, rules, 40);
+    var lowestCount = elementCounts.First().Count;
+    var highestCount = elementCounts.Last().Count;
+    Console.WriteLine($"Part 2: {highestCount - lowestCount}");
 }
 
 LinkedList<char> BuildPolymer(string template, Rule[] rules, int steps)
@@ -83,6 +71,57 @@ void ApplyRules(LinkedList<char> polymer, Dictionary<(char E1, char E2), Rule> r
             node = node.Next;
         }
     }
+}
+
+List<(char Element, long Count)> BuildPolymerElementCounts(string template, Rule[] rules, int steps)
+{
+    var ruleDict = rules.ToDictionary(r => (r.E1, r.E2));
+    var pairCounts =
+        template.Zip(template.Skip(1), (e1, e2) => (E1: e1, E2: e2))
+            .ToDictionary(es => (es.E1, es.E2), _ => 1L);
+
+    for (int i = 0; i < steps; i++)
+    {
+        var updates = new List<(char E1, char E2, long Count)>();
+
+        foreach (var pairCount in pairCounts)
+        {
+            var e1 = pairCount.Key.E1;
+            var e2 = pairCount.Key.E2;
+
+            if (ruleDict.TryGetValue((e1, e2), out var rule))
+            {
+                updates.Add((e1, e2, -pairCount.Value));
+                updates.Add((e1, rule.Insert, pairCount.Value));
+                updates.Add((rule.Insert, e2, pairCount.Value));
+            }
+        }
+
+        foreach (var update in updates)
+        {
+            if (pairCounts.ContainsKey((update.E1, update.E2)))
+            {
+                pairCounts[(update.E1, update.E2)] += update.Count;
+            }
+            else
+            {
+                pairCounts[(update.E1, update.E2)] = update.Count;
+            }
+        }
+    }
+
+    var elementCounts = pairCounts
+        .SelectMany(kvp => new[] { (Element: kvp.Key.E1, Count: kvp.Value), (Element: kvp.Key.E2, Count: kvp.Value) })
+        .GroupBy(elemCount => elemCount.Element, elemCount => elemCount.Count)
+        .ToDictionary(elemGroup => elemGroup.Key, elemGroup => elemGroup.Sum() / 2);
+
+    elementCounts[template[0]]++;
+    elementCounts[template[^1]]++;
+
+    return elementCounts
+        .Select(kvp => (Element: kvp.Key, Count: kvp.Value))
+        .OrderBy(count => count.Count)
+        .ToList();
 }
 
 Part1();
